548512cafad3a3b154a8ae05e2f68a8d1bd34b7a
Ragnar Westad <ragnarwestad@yahoo.no>
2011-09-04 13:53:35 +0200

    Forbedret PropertyChangeTest

---
+++ src/test/java/org/jfsm/core/test/events/IntLt.java
@@ -0,0 +1,44 @@
+package org.jfsm.core.test.events;
+
+import java.beans.PropertyChangeEvent;
+
+import org.jfsm.core.GuardAdapter;
+
+/**
+ * A guard condition for 'less than'. The constructor supplies the limit
+ * value, and the event from the <code>evaluate</code> method is the value to
+ * test against.
+ * 
+ * @author rwe
+ * 
+ */
+public class IntLt extends GuardAdapter {
+
+	private final int intValue;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param pIntValue the limit to test for 'less than'
+	 */
+	public IntLt(final int pIntValue) {
+		this.intValue = pIntValue;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean evaluate(final Object event) {
+
+		if (event instanceof PropertyChangeEvent) {
+			final PropertyChangeEvent pce = (PropertyChangeEvent) event;
+			final Integer newInt = (Integer) pce.getNewValue();
+			if (newInt.intValue() < intValue) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+}
--- src/test/java/org/jfsm/core/test/events/PropertyChangeTest.java
+++ src/test/java/org/jfsm/core/test/events/PropertyChangeTest.java
@@ -11,13 +11,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Test property change events. Set up at <code>TestBean</code> class who emits
- * property change events using standard bean property change mechanisms. Then
- * use a custom JFsm property change event,
- * <code>MyBeanPropertyChangeEvent</code> to catch these and input to the FSM.
- * The <code>IntGt</code> guard condition test the changed value and says 'true'
- * when the value greater than 5 has been set using the <code>setX()</code>
- * method.
+ * Use property change events on a JavaBean to drive the state changes. Set up
+ * at <code>TestBean</code> class who emits property change events using
+ * standard bean property change mechanisms. Then use a JFsm property
+ * change event, <code>FsmPropertyChangeEvent</code> to catch these and input
+ * to the FSM. The <code>IntGt</code> guard condition test the changed value and
+ * says 'true' when the value greater than 5 has been set using the
+ * <code>setX()</code> method.
  * 
  * @author rwe
  */
@@ -29,8 +29,7 @@ public class PropertyChangeTest {
 	@Test
 	public void testPropertyChange() {
 
-		final TestBean testBean = new TestBean();
-
+		// Create states and add to the model
 		final StateI state1 = new State(1);
 		final StateI state2 = new State(2);
 		final JFsmModelI fsmModel = new JFsmModel();
@@ -38,22 +37,31 @@ public class PropertyChangeTest {
 		fsmModel.addState(state2);
 		fsmModel.setInitial(state1);
 
-		final FsmPropertyChangeEvent pce = new MyBeanPropertyChangeEvent(testBean);
-		final GuardConditionI xGt5 = new IntGt(5);
-		state1.addTransition(pce, xGt5, null, state2);
-		state2.addTransition(pce, xGt5, null, state1);
+		// Create a property change listener and register it with the test bean.
+		// Then use the listener as an FSM event
+		final FsmPropertyChangeEvent pce = new FsmPropertyChangeEvent();
+		final TestBean testBean = new TestBean();
+		testBean.addPropertyChangeListener(pce);
+		// Add transitions to the states that makes the state change to state 2 
+		// whenever the set x value are greater than 5 ...
+		state1.addTransition(pce, new IntGt(5), null, state2);
+		// ... or back to state 1 when less than 5
+		state2.addTransition(pce, new IntLt(5), null, state1);
 
+		// Create a state machine and start it
 		final JFsm jFsm = new JFsm(fsmModel);
 		jFsm.start();
+		
 		Assert.assertEquals(state1, jFsm.getCurrentState());
 
+		// Set values on the test bean and assert proper changes in state
 		testBean.setX(4);
 		Assert.assertEquals(state1, jFsm.getCurrentState());
 		testBean.setX(5);
 		Assert.assertEquals(state1, jFsm.getCurrentState());
 		testBean.setX(6);
 		Assert.assertEquals(state2, jFsm.getCurrentState());
-		testBean.setX(7);
+		testBean.setX(4);
 		Assert.assertEquals(state1, jFsm.getCurrentState());
 		testBean.setX(8);
 		Assert.assertEquals(state2, jFsm.getCurrentState());
