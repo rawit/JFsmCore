9c0572b693ad345f3b66712d96e45dcef2ab8cfd
Ragnar Westad <ragnarwestad@yahoo.no>
2011-09-04 13:52:53 +0200

    Removed 'abstract'

--- src/main/java/org/jfsm/core/events/FsmPropertyChangeEvent.java
+++ src/main/java/org/jfsm/core/events/FsmPropertyChangeEvent.java
@@ -12,9 +12,8 @@ import org.jfsm.core.fsm.JFsm;
  * @author rwe
  * 
  */
-public abstract class FsmPropertyChangeEvent extends Event implements PropertyChangeListener {
+public class FsmPropertyChangeEvent extends Event implements PropertyChangeListener {
 
-<<<<<<< HEAD
 	private static final long serialVersionUID = 1L;
 
 	private JFsm jFsm;
@@ -45,7 +44,8 @@ public abstract class FsmPropertyChangeEvent extends Event implements PropertyCh
 	/**
 	 * Set the reference to the JFsm.
 	 * 
-	 * @param jFsm the value
+	 * @param jFsm
+	 *            the value
 	 */
 	public void setJFsm(final JFsm jFsm) {
 		this.jFsm = jFsm;
@@ -75,65 +75,4 @@ public abstract class FsmPropertyChangeEvent extends Event implements PropertyCh
 
 		return this.getClass().getName();
 	}
-=======
-    private static final long serialVersionUID = 1L;
-
-    private JFsm jFsm;
-
-    private boolean running = false;
-
-    /**
-     * Default constructor.
-     */
-    public FsmPropertyChangeEvent() {
-        super(PropertyChangeEvent.class);
-    }
-
-    /**
-     * Start the listener.
-     */
-    public void start() {
-        running = true;
-    }
-
-    /**
-     * Stop the listener.
-     */
-    public void stop() {
-        running = false;
-    }
-
-    /**
-     * Set the reference to the JFsm.
-     * 
-     * @param jFsm the value
-     */
-    public void setJFsm(final JFsm jFsm) {
-        this.jFsm = jFsm;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void propertyChange(final PropertyChangeEvent evt) {
-        if (!running) {
-            return;
-        }
-
-        try {
-            this.jFsm.input(evt);
-        } catch (final JFsmException jfsme) {
-            throw new RuntimeException(jfsme);
-        }
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public String toString() {
-
-        return this.getClass().getName();
-    }
->>>>>>> branch 'master' of git@github.com:rawit/JFsmCore.git
 }
